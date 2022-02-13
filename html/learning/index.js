d1cf926edbf727561811e6ebfbf32dec857946cc
rahulverma0711 <vermamm122@gamil.com>
2022-02-13 21:15:06 +0530

    var let done

---
+++ html/learning/index.js
@@ -0,0 +1,23 @@
+//starting JS-> Javascript
+//var let cont
+
+//variable declaration
+var a;//int string float
+
+
+//variable initalization
+a=10;
+console.log(a);
+
+a="hello";
+console.log(a);
+
+
+//typeof operator ->it tells us the type of data stored in a variable
+console.log(typeof(a));
+
+
+ var num=100;
+ console.log(`half of ${num} is ${num/2}`);
+
+console.log("rahul")
