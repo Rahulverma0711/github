3be30f08e6daad03f631fed52d05c5aad0100390
rahulverma0711 <vermamm122@gamil.com>
2022-02-17 22:16:01 +0530

    object example1

---
+++ html/learning/object.js
@@ -0,0 +1,10 @@
+let obj={};
+console.log(obj);
+let person={
+    name:"Rahul verma",
+    age:21,
+    gender:"male"
+};
+console.log(`hello ${person.name} thank you`);
+console.log(person.age);
+console.log(person["age"]);//square bracket notatiton
