7a3d0bc4380732002e05835ffce4e3c800b19fef
Rahulverma0711 <vermamm122@gmail.com>
2022-03-08 22:48:25 +0530

    wcat project started

---
+++ node/wcat/wcat.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+let inputArr = process.argv.slice(2);
+// console.log(input);
+
+let filesArr = [];
+// placed files path in filesArr
+for (let i = 0; i < inputArr.length; i++){
+    filesArr.push(inputArr[i]);
+}
+// console.log("file to be read are " + filesArr);
+
+//check if all the files are present //
+for (let i = 0; i < filesArr.length; i++){
+    let doesExist = fs.existsSync(filesArr[i]);
+    if (!doesExist) {
+        console.log("Files does not exist ");
+        return;
+    }
+}
+
+// content read and appending starts//
+let content = "";
+for (let i = 0; i < filesArr.length; i++){
+    let fileContent = fs.readFileSync(filesArr[i]);
+    content += fileContent+"\n";
+}
+console.log(content);
