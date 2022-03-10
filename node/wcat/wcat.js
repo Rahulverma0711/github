88f5c4ad39028feafab7fe2d48e1cb81937fba65
Rahulverma0711 <vermamm122@gmail.com>
2022-03-10 22:27:57 +0530

    -scommand done

--- node/wcat/wcat.js
+++ node/wcat/wcat.js
@@ -1,11 +1,20 @@
+const { Console } = require("console");
 const fs = require("fs");
 let inputArr = process.argv.slice(2);
 // console.log(input);
 
 let filesArr = [];
+let optionArr=[];
 // placed files path in filesArr
 for (let i = 0; i < inputArr.length; i++){
-    filesArr.push(inputArr[i]);
+    let firstChar=inputArr[i].charAt(0);
+    if(firstChar=='-'){
+        optionArr.push(inputArr[i]);
+    }else{
+        filesArr.push(inputArr[i]);
+    }
+
+    
 }
 // console.log("file to be read are " + filesArr);
 
@@ -25,3 +34,27 @@ for (let i = 0; i < filesArr.length; i++){
     content += fileContent+"\n";
 }
 console.log(content);
+
+let contentArr=content.split("\n");
+console.log(contentArr);
+
+let isPresent=optionArr.includes("-s");
+if(isPresent){
+    for(let i=1;i<contentArr.length;i++){
+        if(contentArr[i]==""&& contentArr[i-1]==""){
+            contentArr[i]=null;
+        }
+        else if(contentArr[i]==""&& contentArr[i-1]==null){
+            contentArr[i]=null;
+        }
+
+    }
+}
+// console.log(contentArr);
+ tempArr=[];
+ for(let i=0;i<contentArr.length;i++){
+     if(contentArr[i]!=null){
+         tempArr.push(contentArr[i]);
+     }
+ }
+console.log(tempArr);
